d94b6eebfc2b03c92efe5549f7237be9707458a1
John Wregglesworth <wregglej@cyverse.org>
2022-04-25 13:12:43 -0700

    Use Marshal/Unmarshal in Codec methods

--- protobufjson/protobufjson.go
+++ protobufjson/protobufjson.go
@@ -7,37 +7,6 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// Codec is a an implmentation of the NATS Encoder interface that
-// can serialize/deserialize messages using protojson. Some logic is borrowed
-// from the protocol buffer encoder included in NATS.
-// See https://github.com/nats-io/nats.go/blob/main/encoders/protobuf/protobuf_enc.go
-type Codec struct{}
-
-func (c *Codec) Encode(subject string, v interface{}) ([]byte, error) {
-	msg, ok := v.(proto.Message)
-	if !ok {
-		return nil, errors.New("invalid protocol buffer message passed to Encode()")
-	}
-	b, err := protojson.Marshal(msg)
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
-}
-
-func (c *Codec) Decode(subject string, data []byte, vPtr interface{}) error {
-	if _, ok := vPtr.(*interface{}); ok {
-		return nil
-	}
-
-	msg, ok := vPtr.(proto.Message)
-	if !ok {
-		return errors.New("invalid protocol buffer message passed to Decode()")
-	}
-
-	return protojson.Unmarshal(data, msg)
-}
-
 func Unmarshal(data []byte, ptr interface{}) error {
 	if _, ok := ptr.(*interface{}); ok {
 		return nil
@@ -61,4 +30,18 @@ func Marshal(ptr interface{}) ([]byte, error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+// Codec is a an implmentation of the NATS Encoder interface that
+// can serialize/deserialize messages using protojson. Some logic is borrowed
+// from the protocol buffer encoder included in NATS.
+// See https://github.com/nats-io/nats.go/blob/main/encoders/protobuf/protobuf_enc.go
+type Codec struct{}
+
+func (c *Codec) Encode(subject string, v interface{}) ([]byte, error) {
+	return Marshal(v)
+}
+
+func (c *Codec) Decode(subject string, data []byte, vPtr interface{}) error {
+	return Unmarshal(data, vPtr)
 }
