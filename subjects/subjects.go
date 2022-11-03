c6972cb46d7959d878f7047a017d74f991ab1458
John Wregglesworth <wregglej@cyverse.org>
2022-11-03 14:03:38 -0700

    Add a QMS user summary subject

--- subjects/subjects.go
+++ subjects/subjects.go
@@ -7,14 +7,13 @@ const cyverseAnalysis = "cyverse.analysis"
 const cyverseMonitoring = "cyverse.monitoring"
 
 var (
-	QMSGetUserUpdates = fmt.Sprintf("%s.updates.get", qmsUser)
-	QMSAddUserUpdate  = fmt.Sprintf("%s.updates.add", qmsUser)
-
+	QMSGetUserUpdates    = fmt.Sprintf("%s.updates.get", qmsUser)
+	QMSAddUserUpdate     = fmt.Sprintf("%s.updates.add", qmsUser)
 	QMSGetUserOverages   = fmt.Sprintf("%s.overages.get", qmsUser)
 	QMSCheckUserOverages = fmt.Sprintf("%s.overages.check", qmsUser)
-
-	QMSGetUserUsages = fmt.Sprintf("%s.usages.get", qmsUser)
-	QMSAddUserUsages = fmt.Sprintf("%s.usages.add", qmsUser)
+	QMSGetUserUsages     = fmt.Sprintf("%s.usages.get", qmsUser)
+	QMSAddUserUsages     = fmt.Sprintf("%s.usages.add", qmsUser)
+	QMSUserSummary       = fmt.Sprintf("%s.summary.get", qmsUser)
 
 	AnalysisStatus = fmt.Sprintf("%s.status", cyverseAnalysis)
 
