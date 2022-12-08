6ca911be262e487db1fb14d9a875e6afceef1108
John Wregglesworth <wregglej@cyverse.org>
2022-12-08 11:29:11 -0700

    Added more subjects, addede qms sub-package

---
+++ subjects/qms/qms.go
@@ -0,0 +1,22 @@
+package qms
+
+import "fmt"
+
+const qmsUser = "cyverse.qms.user"
+const qmsPlan = "cyverse.qms.plan"
+
+var (
+	GetUserUpdates    = fmt.Sprintf("%s.updates.get", qmsUser)
+	AddUserUpdate     = fmt.Sprintf("%s.updates.add", qmsUser)
+	GetUserOverages   = fmt.Sprintf("%s.overages.get", qmsUser)
+	CheckUserOverages = fmt.Sprintf("%s.overages.check", qmsUser)
+	GetUserUsages     = fmt.Sprintf("%s.usages.get", qmsUser)
+	AddUserUsages     = fmt.Sprintf("%s.usages.add", qmsUser)
+	UserSummary       = fmt.Sprintf("%s.summary.get", qmsUser)
+	AddUser           = fmt.Sprintf("%s.add", qmsUser)
+	GetUserPlan       = fmt.Sprintf("%s.plan.get", qmsUser)
+	ChangeUserPlan    = fmt.Sprintf("%s.plan.change", qmsUser)
+	ListPlans         = fmt.Sprintf("%s.list", qmsPlan)
+	UpdatePlan        = fmt.Sprintf("%s.update", qmsPlan)
+	AddPlan           = fmt.Sprintf("%s.add", qmsPlan)
+)
--- subjects/subjects.go
+++ subjects/subjects.go
@@ -3,17 +3,26 @@ package subjects
 import "fmt"
 
 const qmsUser = "cyverse.qms.user"
+const qmsPlan = "cyverse.qms.plan"
 const cyverseAnalysis = "cyverse.analysis"
 const cyverseMonitoring = "cyverse.monitoring"
 
 var (
-	QMSGetUserUpdates    = fmt.Sprintf("%s.updates.get", qmsUser)
+	QMSGetUserUpdates           = fmt.Sprintf("%s.updates.get", qmsUser)
+	SubscriptionsGetUserUpdates = QMSGetUserUpdates
+
 	QMSAddUserUpdate     = fmt.Sprintf("%s.updates.add", qmsUser)
 	QMSGetUserOverages   = fmt.Sprintf("%s.overages.get", qmsUser)
 	QMSCheckUserOverages = fmt.Sprintf("%s.overages.check", qmsUser)
 	QMSGetUserUsages     = fmt.Sprintf("%s.usages.get", qmsUser)
 	QMSAddUserUsages     = fmt.Sprintf("%s.usages.add", qmsUser)
 	QMSUserSummary       = fmt.Sprintf("%s.summary.get", qmsUser)
+	QMSAddUser           = fmt.Sprintf("%s.add", qmsUser)
+	QMSGetUserPlan       = fmt.Sprintf("%s.plan.get", qmsUser)
+	QMSChangeUserPlan    = fmt.Sprintf("%s.plan.change", qmsUser)
+	QMSListPlans         = fmt.Sprintf("%s.list", qmsPlan)
+	QMSUpdatePlan        = fmt.Sprintf("%s.update", qmsPlan)
+	QMSAddPlan           = fmt.Sprintf("%s.add", qmsPlan)
 
 	AnalysisStatus = fmt.Sprintf("%s.status", cyverseAnalysis)
 
